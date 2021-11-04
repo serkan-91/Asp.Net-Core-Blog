2f767d1605fc6af131ada3a76c9011cbcbca49a7
serkan durgut <36546652+serkan-91@users.noreply.github.com>
2021-11-04 12:27:25 +0300

    navbar link added

---
+++ CoreDemo/CoreDemo/Controllers/ContactController.cs
@@ -0,0 +1,31 @@
+﻿using CoreDemo.BusinessLayer.Service;
+using CoreDemo.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CoreDemo.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ContactController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact p)
+        {
+            p.ContactDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.ContactStatus = true;
+            _unitOfWork.Contacts.Add(p);
+            return RedirectToAction("Index","Blog");
+        }
+    }
+}
